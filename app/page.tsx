01a936d3742b7002d967904b90f3d60e72d14801
Lawrence Caulker <locaulker@gmail.com>
2026-05-13 15:07:38 -0400

    working on static images on the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import HeroSection from "@/components/HeroSection"
+
 export default function Home() {
   return (
     <>
-      <h1>Movie Box</h1>
+      <h1>
+        <HeroSection />
+      </h1>
     </>
   )
 }
