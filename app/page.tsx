b0db81b6a66df5642d5f07c66f4367cc04a07305
Lawrence Caulker <locaulker@gmail.com>
2026-05-14 11:37:28 -0400

    started movie app

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import HeroSection from "@/components/HeroSection"
 
+const TEMP_API_KEY = ""
+
 export default function Home() {
   return (
     <>
-      <h1>
-        <HeroSection />
-      </h1>
+      <HeroSection />
+
+      <div className="flex flex-col m-10 mt-0 ">
+        <div className="mb-10">
+          <h2 className="text-3xl font-bold text-alabaster mb-3">
+            Popular Right Now
+          </h2>
+          <p className="text-lg text-santas-gray">
+            Explore what everyone is watching
+          </p>
+        </div>
+      </div>
     </>
   )
 }
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,27 +4,25 @@ import Image from "next/image"
 const HeroSection = () => {
   return (
     <header className="relative h-[70vh]">
-      <div className="relative h-[70vh]">
-        <div className="w-5/12 z-10 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
-          <div className="flex items-center justify-center flex-col mb-4 gap-4">
-            <div className="flex items-center justify-center w-14 h-14 rounded-xl bg-red-500 shadow-lg mb-4">
-              <Popcorn className="h-7 w-7" />
-            </div>
-            <h1 className="text-7xl text-white font-bold tracking-tight">
-              MovieBox
-            </h1>
-            <p className="text-2xl tracking-tight font-light mb-4 text-santas-gray">
-              Discover the most popular movies trending right now
-            </p>
+      <div className="w-5/12 z-10 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
+        <div className="flex items-center justify-center flex-col mb-4 gap-4">
+          <div className="flex items-center justify-center w-14 h-14 rounded-xl bg-red-500 shadow-lg mb-4">
+            <Popcorn className="h-7 w-7" />
           </div>
-          <div className="relative h-12">
-            <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-santas-gray" />
-            <input
-              className="mb-10 w-full h-full rounded-xl pl-10 pr-10 outline-none border-none bg-dark-black"
-              placeholder="Search movies..."
-              type="text"
-            />
-          </div>
+          <h1 className="text-7xl text-white font-bold tracking-tight">
+            MovieBox
+          </h1>
+          <p className="text-2xl tracking-tight font-light mb-4 text-santas-gray">
+            Discover the most popular movies trending right now
+          </p>
+        </div>
+        <div className="relative h-12">
+          <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-santas-gray" />
+          <input
+            className="mb-10 w-full h-full rounded-xl pl-10 pr-10 outline-none border-none bg-dark-black"
+            placeholder="Search movies..."
+            type="text"
+          />
         </div>
       </div>
 
@@ -44,8 +42,8 @@ const HeroSection = () => {
           ))}
       </div>
 
-      <div className="absolute inset-0 bg-linear-to-t from-woodsmoke via -woodsmooke/80 to-transparent "></div>
-      <div className="absolute inset-0 bg-linear-to-t from-woodsmoke via -woodsmooke/90 via-transparent to-woodsmoke"></div>
+      <div className="absolute inset-0 bg-linear-to-t from-woodsmoke via -woodsmooke/80 to-transparent"></div>
+      <div className="absolute inset-0 bg-linear-to-t from-woodsmoke/90 via-transparent to-woodsmoke/90"></div>
     </header>
   )
 }
